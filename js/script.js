5ef79e826e098922a2a4a7d2f29c54c16b831982
Mokki78 <100276996+Mokki78@users.noreply.github.com>
2022-10-09 22:55:01 +0200

    Updates links and nav

--- js/script.js
+++ js/script.js
@@ -1,22 +1,37 @@
-const colours = ["red", "white", "blue", "green"];
 
-colours.forEach(function(item) {
-    console.log(item);
+const form = document.querySelector(".contact-form");
+const name = document.querySelector("#name");
+const nameError = document.querySelector("#error-name");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#error-email");
+const mobile = document.querySelector("#mobile");
+const mobileError = document.querySelector("#error-mobile");
 
-});
+function validateForm() {
+    Event.preventDefault();
 
-function validateEmail(email) {
-    const regEx = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/;
-    const patternMatches = regEx.test(email);
-    return patternMatches;
-
-    if {
-        validateEmail = true {
-            returnMessage ="You have entered a valid email"
-        }
-        else{
-            returnMessage= "You have not entered a valid email"
-        }
+    if(name.value.length > 0){
+        nameError.style.display = "none";
+    } else(name.value.length == 0);{
+        nameError.style.display = "block";
     }
+    
+    if(email.value.length > 0){
+        emailError.style.display = "none";
+    }else(email.value.length == 0);{
+        emailError.style.display = "block";
+    }
+
+    if(mobile.value.length > 0){
+        mobileError.style.display = "none";
+    }else(mobile.value.length == 0);{
+        mobileError.style.display = "block";
+    }
+        
 }
+
+form.addEventListener("submit", validateForm);
+
+
+
 

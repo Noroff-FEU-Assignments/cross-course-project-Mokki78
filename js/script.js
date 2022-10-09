12a92c70d5136fdd8e5fdb345441afd23f7de9f0
Mokki78 <100276996+Mokki78@users.noreply.github.com>
2022-10-09 16:10:50 +0200

    Thank you js
    thank you js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ colours.forEach(function(item) {
     console.log(item);
 
 });
+
+function validateEmail(email) {
+    const regEx = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/;
+    const patternMatches = regEx.test(email);
+    return patternMatches;
+
+    if {
+        validateEmail = true {
+            returnMessage ="You have entered a valid email"
+        }
+        else{
+            returnMessage= "You have not entered a valid email"
+        }
+    }
+}
+
